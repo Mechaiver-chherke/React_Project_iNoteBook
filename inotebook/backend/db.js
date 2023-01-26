80300ff82f654b9044fe11a1dc5301927b4cba1a
Mechaiver-chherke <chherke24@gmail.com>
2023-01-26 10:02:07 +0530

    first commit

---
+++ inotebook/backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+mongoose.set('strictQuery', true);
+const connectToMongo = ()=> {
+    mongoose.connect(mongoURI, ()=>{
+        console.log("connected bro")
+    })
+}
+module.exports = connectToMongo;
